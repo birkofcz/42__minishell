b219bab0779fad35ff749c1e593538f7ca0ea5ad
Stepan Benes <sbenes@c1r6s4.42prague.com>
2023-05-28 17:21:07 +0200

    echo builtin (c, makefile, crossroads)

--- src/builtin_echo.c
+++ src/builtin_echo.c
@@ -6,11 +6,16 @@
 /*   By: sbenes <sbenes@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 15:07:25 by sbenes            #+#    #+#             */
-/*   Updated: 2023/05/28 16:33:28 by sbenes           ###   ########.fr       */
+/*   Updated: 2023/05/28 17:19:14 by sbenes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+void	ft_echoenv(char *arg)
+{
+	printf("%s", getenv(&arg[1]));
+}
 
 void	ft_echoprint(char **words, int i)
 {
@@ -26,12 +31,16 @@ void	ft_echoprint(char **words, int i)
 void	ft_echo(char **words)
 {
 	//TO SOLVE LATER - case with "echo $USER", it has to print value of env variable.
+	int i;
 
+	i = 0;
 	if (!words[1])
 	{
 		printf("\n");
 		return ;
 	}
+	else if (words[i++][0] == '$')
+		ft_echoenv(words[i]);
 	else if (ft_strncmp(words[1], "-n", ft_strlen(words[1])) == 0)
 		ft_echoprint(words, 2);
 	else
