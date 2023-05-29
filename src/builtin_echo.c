8eaa3422e4baed16526c5d5ac35a1b048f1ebc8e
Stepan Benes <sbenes@c1r6s4.42prague.com>
2023-05-29 15:12:04 +0200

    working on echo

--- src/builtin_echo.c
+++ src/builtin_echo.c
@@ -6,22 +6,27 @@
 /*   By: sbenes <sbenes@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 15:07:25 by sbenes            #+#    #+#             */
-/*   Updated: 2023/05/28 17:19:14 by sbenes           ###   ########.fr       */
+/*   Updated: 2023/05/29 15:11:11 by sbenes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-void	ft_echoenv(char *arg)
+/* void	ft_echoenv(char *arg)
 {
 	printf("%s", getenv(&arg[1]));
-}
+} */
 
 void	ft_echoprint(char **words, int i)
 {
 	while (words[i] != NULL)
 	{
-		printf("%s", words[i]);
+		if (words[i][0] == '"' || words[i][ft_strlen(words[i]) - 1] == '"')
+			words[i] = ft_strtrim(words[i], "\"");
+		if (words[i][0] == '$')
+			printf("%s", getenv(&words[i][1]));
+		else
+			printf("%s", words[i]);
 		if (words[i + 1] != NULL)
 			printf(" ");
 		i++;
@@ -30,17 +35,8 @@ void	ft_echoprint(char **words, int i)
 
 void	ft_echo(char **words)
 {
-	//TO SOLVE LATER - case with "echo $USER", it has to print value of env variable.
-	int i;
-
-	i = 0;
 	if (!words[1])
-	{
 		printf("\n");
-		return ;
-	}
-	else if (words[i++][0] == '$')
-		ft_echoenv(words[i]);
 	else if (ft_strncmp(words[1], "-n", ft_strlen(words[1])) == 0)
 		ft_echoprint(words, 2);
 	else
@@ -48,4 +44,6 @@ void	ft_echo(char **words)
 		ft_echoprint(words, 1);
 		printf("\n");
 	}
+
+
 }
